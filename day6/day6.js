782ee86d43df88758a10bd334f11e670ceb1a75b
Terrence052312 <usa9trina0906332388@gmail.com>
2023-10-16 02:13:29 +0800

    aaaa

--- day6/day6.js
+++ day6/day6.js
@@ -3,4 +3,4 @@ console.log(a_1_$);
 //var var = 123
 //console.log(var)
 // you can't use the name "var" because var repensent's something
-//already so if you used that, there will be an error.
+//already so if you used that, there will be an error
